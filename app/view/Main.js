43b0eee2255cb93ef8e3036b71b25adb9bbb8789
Roel Sint <roel@beeldspraak.com>
2012-05-31 11:17:09 +0200

    updated video url ext.video

--- app/view/Main.js
+++ app/view/Main.js
@@ -36,12 +36,12 @@ Ext.define("BS.view.Main", {
                     {
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
+                        title: 'Video'
                     },
                     {
                         xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        url: 'http://www.longtailvideo.com/jw/upload/bunny.mp4',
+                        posterUrl: 'http://www.longtailvideo.com/jw/upload/bunny.jpg'
                     }
                 ]
             },
